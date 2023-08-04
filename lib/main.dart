e00d77a8b75715c25c97e1f473f03aea6e3b21e1
ZERONE <zerone012@gmail.com>
2023-08-04 09:42:38 +0900

    UI 공부 중. GridView

--- lib/main.dart
+++ lib/main.dart
@@ -29,111 +29,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final postList = [
-    {
-      "title": "Sample Title 1",
-      "color": Colors.red,
-    },
-    {
-      "title": "Sample Title 2",
-      "color": Colors.orange,
-    },
-    {
-      "title": "Sample Title 3",
-      "color": Colors.yellow,
-    },
-    {
-      "title": "Sample Title 4",
-      "color": Colors.green,
-    },
-    {
-      "title": "Sample Title 5",
-      "color": Colors.blue,
-    },
-    {
-      "title": "Sample Title 6",
-      "color": Colors.indigo,
-    },
-    {
-      "title": "Sample Title 7",
-      "color": Colors.purple,
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Test Title')),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text('Drawer Header Part'),
-            ),
-            ListTile(
-              title: Text('Menu 1'),
-            ),
-          ],
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 10,
         ),
-      ),
-      body: ListView.builder(
-        itemCount: postList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return postContainer(
-            title: postList[index]["title"] as String,
-            colorData: postList[index]["color"] as Color,
-          );
-        },
-      ),
-      // body: ListView(
-      //   children: [
-      //     postContainer(
-      //       title: 'test1',
-      //       colorData: Colors.red,
-      //     ),
-      //     postContainer(
-      //       title: 'test2',
-      //       colorData: Colors.orange,
-      //     ),
-      //     postContainer(
-      //       title: 'test3',
-      //       colorData: Colors.yellow,
-      //     ),
-      //     postContainer(
-      //       title: 'test4',
-      //       colorData: Colors.green,
-      //     ),
-      //   ],
-      // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('clicked'),
-        child: const Icon(Icons.mouse),
+        children: [
+          postConatiner(number: "1", colorData: Colors.red),
+          postConatiner(number: "2", colorData: Colors.orange),
+          postConatiner(number: "3", colorData: Colors.yellow),
+          postConatiner(number: "4", colorData: Colors.green),
+          postConatiner(number: "5", colorData: Colors.blue),
+          postConatiner(number: "6", colorData: Colors.indigo),
+          postConatiner(number: "7", colorData: Colors.purple),
+        ],
       ),
     );
   }
 
-  Widget postContainer({
-    String title = '',
-    Color colorData = Colors.blue,
+  Widget postConatiner({
+    String number = "0",
+    Color colorData = Colors.amber,
   }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(10),
-          child: Text(
-            title,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 200,
-          color: colorData,
-        ),
-      ],
+    return Container(
+      height: 200,
+      color: colorData,
+      child: Center(
+        child: Text("Box $number"),
+      ),
     );
   }
 }
