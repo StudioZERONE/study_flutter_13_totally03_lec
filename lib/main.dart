bef35c18ad845e4afbb90f3a82d2c25fbb2d1681
ZERONE <zerone012@gmail.com>
2023-08-16 10:36:16 +0900

    UI 공부 중. bottomNavigationBar, 화면 하단 네비게이션바 배치 및 setState 사용

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _selectedIndex = 0;
+
   late SharedPreferences _prefs;
   String _username = "";
   final TextEditingController _usernameController = TextEditingController();
@@ -38,6 +42,16 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _getUsername();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() => setState(
+          () => _selectedIndex = _tabController.index,
+        ));
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   _saveUsername() {
@@ -58,29 +72,74 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Test App')),
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Text('현재 사용자 이름: $_username'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              child: TextField(
-                controller: _usernameController,
-                textAlign: TextAlign.left,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Input your username',
+      body: _selectedIndex == 0
+          ? tabContainer(context, Colors.indigo, "Friedns Tab")
+          : _selectedIndex == 1
+              ? tabContainer(context, Colors.amber, "Chats Tab")
+              : Container(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Text('현재 사용자 이름: $_username'),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        child: TextField(
+                          controller: _usernameController,
+                          textAlign: TextAlign.left,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Input your username',
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () => _saveUsername(),
+                        child: const Text('저장'),
+                      ),
+                    ],
+                  ),
                 ),
+      bottomNavigationBar: SizedBox(
+        height: 100,
+        child: TabBar(
+          controller: _tabController,
+          labelColor: Colors.black,
+          tabs: [
+            Tab(
+              icon: Icon(
+                _selectedIndex == 0 ? Icons.person : Icons.person_2_outlined,
               ),
+              text: "Friends",
             ),
-            TextButton(
-              onPressed: () => _saveUsername(),
-              child: const Text('저장'),
+            Tab(
+              icon: Icon(
+                _selectedIndex == 1 ? Icons.chat : Icons.chat_outlined,
+              ),
+              text: "Chats",
+            ),
+            Tab(
+              icon: Icon(
+                _selectedIndex == 2 ? Icons.settings : Icons.settings_outlined,
+              ),
+              text: "Settings",
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget tabContainer(BuildContext con, Color tabColor, String tabText) {
+    return Container(
+      width: MediaQuery.of(con).size.width,
+      height: MediaQuery.of(con).size.height,
+      color: tabColor,
+      child: Center(
+        child: Text(
+          tabText,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
     );
